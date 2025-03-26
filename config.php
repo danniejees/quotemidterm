18ee3451ea9bfb66eb3a9b79b3815cbe49b7e963
dal <dalantonjam@gmail.com>
2025-03-26 17:45:56 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,17 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$host = getenv('DB_HOST');       
+$port = getenv('DB_PORT');      
+$dbname = getenv('DB_NAME');     
+$user = getenv('DB_USER');       
+$password = getenv('DB_PASSWORD'); 
 
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
 try {
     $pdo = new PDO($dsn, $user, $password);
+    
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     die();
