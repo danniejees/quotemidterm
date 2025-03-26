532f5a3eaf0564e68a6553c604e3fac417f2ad90
dal <dalantonjam@gmail.com>
2025-03-26 18:10:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
+
 require 'config.php';
 
 $uri = $_SERVER['REQUEST_URI'];
