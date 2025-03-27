97c29710e88434fd7c11f8d2e53c04ec5daaa4a4
dal <dalantonjam@gmail.com>
2025-03-26 22:14:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
